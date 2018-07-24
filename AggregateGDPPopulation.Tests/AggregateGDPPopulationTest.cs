a1e7d5384b669e4a2d77b5e2e3402977bfd36c54
Nikita karanpuria <nikita.karanpuria8@gmail.com>
2018-07-24 12:22:17 +0530

    done

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -1,14 +1,21 @@
 using System;
 using Xunit;
+using AggregateGDPPopulation;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace AggregateGDPPopulation.Tests
 {
     public class UnitTest1
     {
+
         [Fact]
         public void Test1()
-        {
-
-        }
-    }
+        {  
+             Class1.Aggregate();
+            string s = File.ReadAllText("../../../../AggregateGDPPopulation/output/output.json");
+            string str = File.ReadAllText("../../../expected-output.json");
+        Assert.Equal(s, str);
+         }
 }
+ }
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -1,8 +1,55 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace AggregateGDPPopulation
 {
+    public class CountryGDPPop
+    {
+        public float GDP_2012 = 0;
+        public float POPULATION_2012 = 0;
+    }
     public class Class1
     {
+        public static void Aggregate()
+        {
+            Dictionary<string, CountryGDPPop> result = new Dictionary<string, CountryGDPPop>();
+            try
+            {
+                string filename = @"../../../../AggregateGDPPopulation/data/datafile.csv";
+                string[] contents = File.ReadAllText(filename).Replace("\"", string.Empty).Split('\n');
+                // Dictionary<String, String> countryContinentMapper;
+                string[] headerRows = contents[0].Split(',');
+                Dictionary<string,string> mapper = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(@"../../../../AggregateGDPPopulation/continent.json"));
+                int indexOfCountry = Array.IndexOf(headerRows, "Country Name");
+                int indexOfPopulation = Array.IndexOf(headerRows, "Population (Millions) 2012");
+                int indexOfGDP = Array.IndexOf(headerRows, "GDP Billions (USD) 2012");
+                for (int i = 1; i < contents.Length; i++)
+                {
+                    string[] row = contents[i].Replace("\"", "").Split(',');
+                    string nameOfContinent = mapper[row[indexOfCountry]];
+                    if (!result.ContainsKey(nameOfContinent)) {
+                        result.Add(nameOfContinent, new CountryGDPPop());
+
+                    }
+                    result[nameOfContinent].GDP_2012 += float.Parse(row[indexOfGDP]);
+                    result[nameOfContinent].POPULATION_2012 += float.Parse(row[indexOfPopulation]);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            string json = JsonConvert.SerializeObject(result,Formatting.Indented);
+            File.WriteAllText(@"../../../../AggregateGDPPopulation/output/output.json", json);
+
+        }
     }
+   
+
 }
